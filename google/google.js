a9ae4f4dcb432348682ed1fe638faa685dbd5b1c
clarkwmcd <clarkwmcd@gmail.com>
2018-01-29 22:11:06 -0500

    added firebase auth to html file

--- google/google.js
+++ google/google.js
@@ -1,9 +1,15 @@
-//clark testing
+$(document).ready(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB0bp-Zgou2JX35373fAVqBF1YrXXp-Jrc",
+    authDomain: "groupproject-1.firebaseapp.com",
+    databaseURL: "https://groupproject-1.firebaseio.com",
+    projectId: "groupproject-1",
+    storageBucket: "groupproject-1.appspot.com",
+    messagingSenderId: "893954563598"
+  };
+  firebase.initializeApp(config);
 
-var code = 1
 
-$(document).ready(function()
-  {
-
-  }
+  
 });
